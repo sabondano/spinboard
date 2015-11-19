97c987b010e05a9d583b1961d8355b5367a3c680
Sebastian Abondano <sabondano1@gmail.com>
2015-11-18 17:55:37 -0700

    Fix mailer bug that was trying to send email to link that was being shared instead of email

--- app/mailers/link_recommender.rb
+++ app/mailers/link_recommender.rb
@@ -3,7 +3,7 @@ class LinkRecommender < ApplicationMailer
 
   def send_recommendation_email(email_data)
     @email_data = email_data
-    mail(to: @email_data[:link],
+    mail(to: @email_data[:to_email],
          subject: 'Link Recommendation from Spinboard')
   end
 end
