12c7d706e0518a36630812dc33f4265fbe3203b7
Sebastian Abondano <sabondano1@gmail.com>
2015-11-18 20:24:18 -0700

    Fix bug on when changing link status with ajax

--- app/assets/javascripts/change_read_status.js
+++ app/assets/javascripts/change_read_status.js
@@ -1,26 +1,18 @@
 $(document).on('ready page:load', function() {
   $(".btn-mark-as-read").on("ajax:success", function(event, data, status, xhr) {
     var $link = $("#"+data.id);
-    $link.children(".title, .url")
-         .removeClass("read-false")
-         .addClass("read-true");
-
-    $link.children(".read-status")
-         .text("true");
-
+    $link.removeClass("link-read-false").addClass("link-read-true");
+    $link.children(".title, .url").removeClass("read-false").addClass("read-true");
+    $link.children(".read-status").text("true");
     $link.find(".btn-mark-as-read").hide();
     $link.find(".btn-mark-as-unread").show();
   }); 
 
   $(".btn-mark-as-unread").on("ajax:success", function(event, data, status, xhr) {
     var $link = $("#"+data.id);
-    $link.children(".title, .url")
-         .removeClass("read-true")
-         .addClass("read-false");
-
-    $link.children(".read-status")
-         .text("false");
-
+    $link.removeClass("link-read-true").addClass("link-read-false");
+    $link.children(".title, .url").removeClass("read-true").addClass("read-false");
+    $link.children(".read-status").text("false");
     $link.find(".btn-mark-as-unread").hide();
     $link.find(".btn-mark-as-read").show();
   }); 
